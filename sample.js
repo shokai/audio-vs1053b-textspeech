48a4f71b9a017b5b568d6b1001d5588875437e0a
Sho Hashimoto <hashimoto@shokai.org>
2014-10-21 06:54:18 +0900

    fix sample

--- sample.js
+++ sample.js
@@ -1,6 +1,8 @@
 var tessel = require('tessel');
 var wifi = require('wifi-cc3000');
 var audio = require('audio-vs1053b').use(tessel.port['A']);
+
+// var textspeech = require('audio-vs1053b-textspeech').use(audio);
 var textspeech = require('./').use(audio);
 
 var led_green = tessel.led[0].output(1);
